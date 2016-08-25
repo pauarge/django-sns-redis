cf234e511cf8086a1aea93b58d453e9ed7ba7b68
Pau Argelaguet <pau@outlook.com>
2016-08-25 13:51:47 +0200

    Working on publishing

--- snsredis/models.py
+++ snsredis/models.py
@@ -1,17 +1,23 @@
 from boto import sns
+from django.conf import settings
+from django_redis import get_redis_connection
 
 VALID_PLATFORMS = ['apns', 'gcm']
 
 
 class UserManager(object):
-    def __init__(self, redis, user_id=None):
-        self.redis = redis
+    def __init__(self, user_id=None):
+        self.redis = get_redis_connection('default')
+        region = sns.connect_to_region(settings.AWS_SNS_REGION_NAME).region
+        self.conn = sns.SNSConnection(region=region, aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+                                      aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
         self.user_id = user_id
 
-    # Returns a dict with "apns" and "gcm" as keys.
-    # Each key contains all endpoints associated with self.user_id
+    # Returns two dicts with apns and gcm endpoints.
     def get_endpoints(self):
-        raise NotImplementedError
+        _hash_apns = 'sns-endpoint:{}:apns'.format(self.user_id)
+        _hash_gcm = 'sns-endpoint:{}:gcm'.format(self.user_id)
+        return self.redis.smembers(_hash_apns), self.redis.smembers(_hash_gcm)
 
     def get_user(self):
         return self.user_id
@@ -22,11 +28,17 @@ class UserManager(object):
     def add_token(self, token, platform=None):
         # TODO: Token check
         if platform in VALID_PLATFORMS:
-            # Boto request
-            # save return to redis
-            pass
+            if platform == 'apns':
+                arn = settings.AWS_SNS_APNS_ARN
+                _hash = 'sns-endpoint:{}:apns'.format(self.user_id)
+            else:
+                arn = settings.AWS_SNS_GCM_ARN
+                _hash = 'sns-endpoint:{}:gcm'.format(self.user_id)
+            ret = self.conn.create_platform_endpoint(platform_application_arn=arn, token=token)
+            arn = ret.get('CreatePlatformEndpointResponse').get('CreatePlatformEndpointResult').get('EndpointArn')
+            self.redis.sadd(_hash, arn)
 
 
 class TopicManager(object):
-    def __init__(self, redis=None):
+    def __init__(self):
         raise NotImplementedError
--- snsredis/tasks.py
+++ snsredis/tasks.py
@@ -1,4 +1,28 @@
+from boto import sns
+
+from django.conf import settings
 from models import UserManager
 
-def add_token(user_id, platform):
-    pass
+
+def add_token(user_id, token, platform):
+    manager = UserManager(user_id)
+    manager.add_token(token, platform)
+
+
+def publish(user_id, message=None, extra=None):
+    manager = UserManager(user_id)
+    apns, gcm = manager.get_endpoints()
+
+    region = sns.connect_to_region(settings.AWS_SNS_REGION_NAME).region
+    conn = sns.SNSConnection(aws_access_key_id=settings.AWS_ACCESS_KEY_ID, region=region,
+                             aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
+
+    if len(apns) > 0:
+        apns_message = ''
+        for e in apns:
+            conn.publish(target_arn=e, message=apns_message, message_structure='json')
+
+    if len(gcm) > 0:
+        gcm_message = ''
+        for e in gcm:
+            conn.publish(target_arn=e, message=gcm_message, message_structure='json')
