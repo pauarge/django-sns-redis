536d638e229fff0851204e95d34e28eae2d7ad16
Pau Argelaguet <pau@outlook.com>
2016-08-29 14:34:10 +0200

    preparing for pypi

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='DjangoSNSRedis',
+    version='0.1dev',
+    packages=['snsredis',],
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
