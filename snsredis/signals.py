f6b4ff86d8196150d4c308fc8379f11c011a9707
Pau Argelaguet <pau@outlook.com>
2016-08-30 17:39:21 +0200

    Moved to lists instead of sets (memory efficency)

--- snsredis/signals.py
+++ snsredis/signals.py
@@ -10,10 +10,10 @@ def add_token_to_redis(sender, instance=None, created=False, **kwargs):
     if created:
         redis = get_redis_connection()
         hash_arn = 'sns-endpoints:{}'.format(instance.user_id)
-        redis.sadd(hash_arn, instance.arn)
+        redis.lpush(hash_arn, instance.arn)
 
 
 @receiver(post_delete, sender=SNSToken)
 def remove_token_from_redis(sender, instance=None, **kwargs):
     redis = get_redis_connection()
-    redis.srem('sns-endpoints:{}'.format(instance.user_id), instance.arn)
+    redis.lrem('sns-endpoints:{}'.format(instance.user_id), instance.arn)
