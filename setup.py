8311d595ba610912c4cdff1ee7e19262085d3c58
Pau Argelaguet <pau@outlook.com>
2016-08-31 11:58:55 +0200

    New release to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-sns-redis',
-    version='0.1',
+    version='0.1.1',
     packages=['snsredis',],
     license='MIT License',
     long_description=open('README.md').read(),
