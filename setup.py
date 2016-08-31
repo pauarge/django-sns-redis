9189ab9a3eca6d09cf1494535ed968d1aa4e66ac
Pau Argelaguet <pau@outlook.com>
2016-08-31 18:22:50 +0200

    Fixing errors of installation

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ from distutils.core import setup
 
 setup(
     name='django-sns-redis',
-    version='0.1.3',
+    version='0.1.4',
     packages=['snsredis',],
     license='MIT License',
     description='Simple package for sending push notifications through AWS SNS using Django and Redis',
-    long_description=open('README.md').read(),
 )
